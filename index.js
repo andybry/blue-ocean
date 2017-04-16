615cfdba7ad730ab41681e79ceecee48f814c1af
andybry <arbryant101@gmail.com>
2017-04-16 21:35:15 +0100

    Try updating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const token = process.env.secret_key;
 
+console.log('user data:')
 axios('https://api.github.com/user', {
     headers: {
         authorization: `token ${token}`
